8713ef90c9a98a01672c05c461335a790dff0855
Administrator <admin@example.com>
2024-02-12 09:57:21 +0000

    Update file update_pc.sh

---
+++ update_pc.sh
@@ -0,0 +1,29 @@
+DIR=/var/www/years/2024/02
+
+I1=$DIR/internal/asian_all_other_countries
+I2=$DIR/internal/latin_america_europe
+I3=$DIR/internal/open_vet
+E1=$DIR/external/asian_all_other_countries
+E2=$DIR/external/latin_america_europe
+E3=$DIR/external/open_vet
+
+rm -rf $I1
+rm -rf $I2
+rm -rf $I3
+rm -rf $E1
+rm -rf $E2
+rm -rf $E3
+
+REACT_APP_PC=i1 npm run build
+mv build $I1
+REACT_APP_PC=i2 npm run build
+mv build $I2
+REACT_APP_PC=i3 npm run build
+mv build $I3
+
+REACT_APP_PC=e1 npm run build
+mv build $E1
+REACT_APP_PC=e2 npm run build
+mv build $E2
+REACT_APP_PC=e3 npm run build
+mv build $E3
