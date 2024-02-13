f1eea4d4dab4cfec3c24aa760c190ad0bdea3e52
Alfonso Prado <support@ihbrisbane.com.au>
2024-02-13 12:20:29 +1000

    update update_pc.sh

--- update_pc.sh
+++ update_pc.sh
@@ -9,23 +9,22 @@ E1=$DIR/external/ApplyVET
 E2=$DIR/external/nQkuQcXaNC0ymC1gsCA9
 E3=$DIR/external/yqofYnOlpNYqofYn
 
-rm -rf $I1
-rm -rf $I2
-rm -rf $I3
-rm -rf $E1
-rm -rf $E2
-rm -rf $E3
-
 NODE_ENV=production REACT_APP_PC=internal_asian npm run build
+rm -rf $I1
 mv build $I1
 NODE_ENV=production REACT_APP_PC=internal_latin npm run build
+rm -rf $I2
 mv build $I2
 NODE_ENV=production REACT_APP_PC=internal_open_vet npm run build
+rm -rf $I3
 mv build $I3
 
 NODE_ENV=production REACT_APP_PC=external_asian npm run build
+rm -rf $E1
 mv build $E1
 NODE_ENV=production REACT_APP_PC=external_latin npm run build
+rm -rf $E2
 mv build $E2
 NODE_ENV=production REACT_APP_PC=external_open_vet npm run build
+rm -rf $E3
 mv build $E3
