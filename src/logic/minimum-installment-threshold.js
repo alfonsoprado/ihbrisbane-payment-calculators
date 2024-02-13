b9563aa4676aa468ea461d75619eed530edafa45
Alfonso Prado <support@ihbrisbane.com.au>
2024-02-14 09:34:56 +1000

    parameterize minimum installment tuition threshold variable

--- src/logic/minimum-installment-threshold.js
+++ src/logic/minimum-installment-threshold.js
@@ -1,8 +1,10 @@
 export function minimumInstallmentThreshold(data, rows) {
+    const minimum_installment_tuition_threshold = data?.payment_calculator?.minimum_installment_tuition_threshold;
+    console.log("mitt", minimum_installment_tuition_threshold);
     const result = [];
     for (let i = 0; i < rows.length; i++) {
         if(rows[i]?.code === "tuition_installment") {
-            if(rows[i-1]?.code === "tuition_installment" && rows[i].paymentAmount < 100) {
+            if(rows[i-1]?.code === "tuition_installment" && rows[i].paymentAmount < minimum_installment_tuition_threshold) {
                 rows[i-1].paymentAmount += rows[i].paymentAmount;
             } else {
                 result.push(rows[i]); 
