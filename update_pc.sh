c6e6005da502caad55448e1800f8ca774417c1dd
Alfonso Prado <support@ihbrisbane.com.au>
2024-04-12 13:21:59 +1000

    add deploy production configurations for aged care

--- update_pc.sh
+++ update_pc.sh
@@ -6,11 +6,15 @@ I1=$DIR/internal/asian-and-all-other-countries-vet
 I2=$DIR/internal/latin-america-and-europe-vet
 I3=$DIR/internal/open-vet
 I4=$DIR/internal/latin-america-and-europe-elicos
+I5=$DIR/internal/asian-and-all-other-countries-aged-care
+I6=$DIR/internal/latin-america-and-europe-aged-care
 
 E1=$DIR/external/ApplyVET
 E2=$DIR/external/latin-america-and-europe-vet-onshore
 E3=$DIR/external/yqofYnOlpNYqofYn
 E4=$DIR/external/latin-america-and-europe-elicos-onshore
+E5=$DIR/external/aged-care
+E6=$DIR/external/latin-america-europe-aged-care
 
 NODE_ENV=production REACT_APP_PC=internal_asian_vet npm run build
 rm -rf $I1
@@ -24,6 +28,12 @@ mv build $I3
 NODE_ENV=production REACT_APP_PC=internal_latin_america_europe_elicos npm run build
 rm -rf $I4
 mv build $I4
+NODE_ENV=production REACT_APP_PC=internal_asian_aged_care npm run build
+rm -rf $I5
+mv build $I5
+NODE_ENV=production REACT_APP_PC=internal_latin_america_europe_aged_care npm run build
+rm -rf $I6
+mv build $I6
 
 NODE_ENV=production REACT_APP_PC=external_asian_vet npm run build
 rm -rf $E1
@@ -37,3 +47,9 @@ mv build $E3
 NODE_ENV=production REACT_APP_PC=external_latin_america_europe_elicos npm run build
 rm -rf $E4
 mv build $E4
+NODE_ENV=production REACT_APP_PC=external_asian_aged_care npm run build
+rm -rf $E5
+mv build $E5
+NODE_ENV=production REACT_APP_PC=external_latin_america_europe_aged_care npm run build
+rm -rf $E6
+mv build $E6
