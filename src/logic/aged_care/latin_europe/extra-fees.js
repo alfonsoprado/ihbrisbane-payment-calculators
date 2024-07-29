8f4c0c62fa7a67845a23d8b1381f8422a735f8da
Alfonso Prado <a.p@ihbrisbane.com>
2024-07-29 13:12:17 +1000

    Fix: Error Material Fee

--- src/logic/aged_care/latin_europe/extra-fees.js
+++ src/logic/aged_care/latin_europe/extra-fees.js
@@ -2,13 +2,11 @@ import { formatDate, findFridayOfPreviousWeeks } from "../../../helpers/dates";
 
 export function latinAmericaEuropeGenerateExtraFeesAgedCare(data, paymentType, courses, specialCasesSelected) {
   const firstCourse = courses[0];
-  const courseName = firstCourse?.coursePricing?.course?.name;
   // Dates
   const courseStartDate = firstCourse?.startDate;
   const courseSecondPaymentDate = formatDate(findFridayOfPreviousWeeks(courseStartDate, 1));
   // Fees
   const enrolmentFee = firstCourse?.coursePricing?.enrolment_fee;
-  const materialFee = firstCourse?.coursePricing?.material_fee;
 
   let extraFees = [
     {
@@ -24,13 +22,6 @@ export function latinAmericaEuropeGenerateExtraFeesAgedCare(data, paymentType, c
       courseName: "",
       paymentAmount: specialCasesSelected?.includes('pfw') || paymentType === 'pay_upfront' ? 0 : data?.payment_plan_fee,
       code: 'payment_plan_fee' 
-    },
-    {
-      dueDate: courseSecondPaymentDate,
-      feeDescription: "Material Fee",
-      courseName: courseName,
-      paymentAmount: specialCasesSelected?.includes('mw') ? 0 : materialFee,
-      code: 'material_fee'
     }
   ];
 
